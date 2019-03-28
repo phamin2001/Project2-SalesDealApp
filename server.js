9db956e89f7e5edca4c6cc3b817829288bc64255
phamin2001 <amin.pahlavani@gmail.com>
2019-03-28 13:09:40 -0600

    update server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express      =   require('express');
-const app          =   express();
-const bodyParser   =   require('body-parser');
-const cors         =   require('cors');
-const session      =   require('express-session');
-const morgan       =   require('morgan');
+const express          =   require('express');
+const app              =   express();
+const bodyParser       =   require('body-parser');
+const session          =   require('express-session');
+const morgan           =   require('morgan');
+const methodOverride   =   require('method-override');
 
 require('dotenv').config();
 require('./db/db');
@@ -21,6 +21,42 @@ store.on('error', function(error) {
 app.use(session({
     secret             :   'this is a random string',
     resave             :   false, 
-    saveUninitialized  :   false
+    saveUninitialized  :   false,
+    store              :   store
 }));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('short'));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+const authsController   =  require('./controllers/authsController');
+const brandsController  =  require('./controllers/brandsController');
+const dealsController   =  require('./controllers/dealsController');
+const usersController   =  require('./controllers/usersController');
+
+app.use('/auths', authsController);
+app.use('/brands', brandsController);
+app.use('/deals', dealsController);
+app.use('/users', usersController);
+
+app.use('/users/:id/brands', (req, res, next) => {
+    req.userId = req.params.id;
+    next();
+}, brandsController);
+app.use('/brands/:id/deals', (req, res, next) => {
+    req.brandId = req.param.id;
+    next();
+}, dealsController);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        message: req.session.message
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server Is Runing!');
+});
+
 
