2a7b48efdb900720daf7e3fdfa324f05eca244eb
phamin2001 <amin.pahlavani@gmail.com>
2019-03-29 18:31:50 -0600

    finish login.ejs

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -3,5 +3,40 @@ const router    =   express.Router();
 const bcrypt    =   require('bcryptjs');
 const User      =   require('../models/user');
 
+// login
+router.get('/login', async (req, res) => {
+    try {
+        res.render('auths/login.ejs');
+    } catch (err) {
+        console.log(err);
+        res.send(err);    
+    }
+});
+
+// login
+router.post('/login', async (req, res) => {
+    try {
+        const foundUser = await User.findOne({'username': req.body.username});
+        if(foundUser) {
+            if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+                req.session.message     =   '';
+                req.session.username    =   foundUser.username;
+                req.session.userId      =   foundUser._id;
+                req.session.logged      =   true;
+                res.redirect(`/users/${foundUser._id}`);
+            } else {
+                console.log('Password is wrong');
+                req.session.message = 'Password is wrong';
+                res.redirect('/');
+            }
+        } else {
+            req.session.message = 'User not found';
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+});
 
 module.exports = router;
