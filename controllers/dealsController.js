28c278300938c27f4967668b2a9312321d89614e
phamin2001 <amin.pahlavani@gmail.com>
2019-04-05 19:58:18 -0600

    working on dealController

--- controllers/dealsController.js
+++ controllers/dealsController.js
@@ -1,8 +1,29 @@
 const express   =   require('express');
 const router    =   express.Router();
-const Deal      =   require('../models/deal');
+const percent      =   require('../models/percent');
 const Brand     =   require('../models/brand');
 
-
+const brandsTitles = [
+    {name: 'Nike'     , percent: '23%' ,  category: 'sport'       },
+    {name: 'Gap'      , percent: '5%'  ,  category: 'clothes'     },
+    {name: 'Starbucks', percent: '15%' ,  category: 'coffee'      },
+    {name: 'Subway'   , percent: '20%' ,  category: 'coffee'      },
+    {name: 'Lacost'   , percent: '30%' ,  category: 'food'        },
+    {name: 'Target'   , percent: '18%' ,  category: 'clothes'     },
+    {name: 'Apple'    , percent: '25%' ,  category: 'clothes'     },
+    {name: 'Samsung'  , percent: '30%' ,  category: 'technology'  },
+    {name: 'Toyota'   , percent: '32%' ,  category: 'auto'        },
+    {name: 'H&M'      , percent: '45%' ,  category: 'clothes'     },
+    {name: 'Ikea'     , percent: '16%' ,  category: 'home'        },
+    {name: 'Laws'     , percent: '32%' ,  category: 'utility'     },
+    {name: 'Zara'     , percent: '12%' ,  category: 'clothes'     },
+    {name: 'Budweiser', percent: '21%' ,  category: 'beer'        },
+    {name: 'Canon'    , percent: '16%' ,  category: 'Camera'      },
+    {name: 'Gucci'    , percent: '30%' ,  category: 'fashion'     },
+    {name: 'Adidas'   , percent: '22%' ,  category: 'sport'       },
+    {name: 'Lego'     , percent: '54%' ,  category: 'game'        },
+    {name: 'KFC'      , percent: '20%' ,  category: 'food'        },
+    {name: 'Lenovo'   , percent: '12%' ,  category: 'computer'    }
+]
 
 module.exports = router;
--- models/deal.js
+++ models/deal.js
@@ -4,6 +4,7 @@ const Brand    = require('./brand');
 const DealSchema = new mongoose.Schema({
     percent    :    {type: Number, required: true},
     category   :    {type: String, required: true},
+    name       :    {type: String},
     // brands     :    [Brand.schema]
     brands     :    [{type: mongoose.Schema.Types.ObjectId, 
                       ref: 'Brand'}]
